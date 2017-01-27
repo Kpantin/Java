3333ba0af901468e62107b35ad6622ad1ac5c71e
Kpantin <kirkpantin@gmail.com>
2017-01-27 09:21:37 -0500

    Update Craps.java

--- Craps.java
+++ Craps.java
@@ -1,40 +1,57 @@
+import java.util.Scanner;
 
-public class Craps {
+public class ExerciseCraps {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		int number1 = (int)(Math.random() * 7);
 		int number2 = (int)(Math.random() * 7);
 		int answer = (number1 + number2);
 		int point1 = (int)(Math.random() * 7);
 		int point2 = (int)(Math.random() * 7);
 		int answer2 = (point1 + point2);
-		
-		System.out.print(number1 + " + " + number2 + " = " + answer);
+
+			
+		System.out.println("You rolled " + number1 + " + " + number2 + " = " + answer);
 	
 		if (answer == 7 || answer == 11) {
-			System.out.println("\nYou Win!");
+			System.out.println("\nYou Win!" + " Natural! (7 or 11)");
 		}
 		
 		else if (answer == 2 || answer == 3 || answer == 12) {
-			System.out.println("\nYou Lose!");
+			System.out.println("\nYou Lose!" + " Craps! (2, 3, or 12)");
 		}
 		
 		else {
-			System.out.println("\nPoint is " + answer);
-			System.out.print(point1 + " + " + point2 + " = " + answer2);
+			System.out.println("Point is " + answer);
+			System.out.println("You rolled " + point1 + " + " + point2 + " = " + answer2);
 			
-			if (answer2 == 7){
-				System.out.println("\nYou Lose!");
+			if (answer2 == 7) {
+				System.out.println("\nYou Lose!" + " After 'Point' 7 is a loss.");
+				input.nextInt();
+			}	
+			
+			if (answer2 == answer) {
+				System.out.println("\nYou Win!" + " Point is " + answer);
+				input.nextInt();
 			}
 			
-			else if (answer2 == answer) {
-				System.out.println("\nYou Win!");
-			}
-			
-			else {
-				System.out.println("\nYou Lose!");	
+			while (answer2 != answer) {
+				int newnum = (int)(Math.random() * 7);
+				int newnum2 = (int)(Math.random() * 7);
+				int answer3 = (newnum + newnum2);
+				System.out.println("You rolled " + newnum + " + " + newnum2 + " = " + answer3);
+				
+				if (answer3 == answer) {
+					System.out.println("\nYou Win!" + " Point is " + answer);
+					input.nextInt();
+				}
+				if (answer3 == 7) {
+					System.out.println("\nYou Lose!" + " After 'Point' 7 is a loss.");
+					input.nextInt();
+				}		
 			}
 		}
-	
+		input.close();
 	}
 }
